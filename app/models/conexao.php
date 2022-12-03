72ff627c969f4624a2627e24e601a8d000ef5500
Jorge Silva <90664016+JorgeSilvva@users.noreply.github.com>
2022-12-03 15:28:51 -0300

    Update conexao.php

--- app/models/conexao.php
+++ app/models/conexao.php
@@ -6,8 +6,3 @@ define('DB', 'helpmaxi');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
 
-
-//HOST: localhost
-//USUARIO: id19945507_root	
-//DB: id19945507_helpmaxi	
-//senha: 6APa(1~?xs3Z?y-a

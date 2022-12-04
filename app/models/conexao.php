393a34eb71922788c0154c667fbe57f7c7fc5a62
Jorge Silva <90664016+JorgeSilvva@users.noreply.github.com>
2022-12-04 16:59:53 -0300

    Update conexao.php

--- app/models/conexao.php
+++ app/models/conexao.php
@@ -1,8 +1,8 @@
 <?php
-define('HOST', 'https://databases.000webhost.com/?_ga=2.172618979.1157770098.1670180700-341334833.1669919242');
-define('USUARIO', 'id19945507_root');
-define('SENHA', '/\!\W-%tgfI^9#8!');
-define('DB', 'id19945507_root');
+define('HOST', '');
+define('USUARIO', '');
+define('SENHA', '');
+define('DB', '');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
 

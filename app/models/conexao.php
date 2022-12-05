470ec7cfc9933e10c018b00dea39a0cb8838511d
Jorge Silva <90664016+JorgeSilvva@users.noreply.github.com>
2022-12-04 23:06:15 -0300

    Update conexao.php

--- app/models/conexao.php
+++ app/models/conexao.php
@@ -1,7 +1,7 @@
 <?php
 define('HOST', 'helpmaxi-servidor.mysql.database.azure.com');
 define('USUARIO', 'hmuqkcfgrk');
-define('SENHA', 'zcr@d2022');
+define('SENHA', '33az481c-');
 define('DB', 'helpmaxi');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');

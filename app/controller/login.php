4ff2f9107c14a0cd99e531a2e86c85881e3792b6
Jorge Silva <90664016+JorgeSilvva@users.noreply.github.com>
2022-12-15 15:47:44 -0300

    Update login.php

--- app/controller/login.php
+++ app/controller/login.php
@@ -23,12 +23,11 @@ include('../models/conexao.php');
 		$_SESSION['nivel'] = $resultado['nivel'];
 		if($_SESSION['nivel'] == "cliente"){
 			header("Location: ../views/dashboardCliente.php");
-			exit();
-			}elseif($_SESSION['nivel'] == "admin"){
-				header("Location: ../views/dashboardAdm.php");
-			}else{
+		}elseif($_SESSION['nivel'] == "admin"){
+			header("Location: ../views/dashboardAdm.php");
+		}else{
 			header('Location: /../index.php');
-			exit();
+		exit();
 		}
 	}else{  
 	$_SESSION['nao_autenticado'] = true;

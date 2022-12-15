9d9779881d551c8c8625d52b690b6bda3cdf2f62
Jorge Silva <90664016+JorgeSilvva@users.noreply.github.com>
2022-12-15 13:53:45 -0300

    Update ticketSave.php

--- app/controller/ticketSave.php
+++ app/controller/ticketSave.php
@@ -4,14 +4,15 @@
     }
 
     include('../models/conexao.php');
+    include('apiEmail.php');
     
     if(isset($_POST['update']))
     {
         $ticket_id = $_POST['ticket_id'];
 
-        $frase = "Ticket atualizado, e-mail enviado com sucesso";
-        $_SESSION['frase'] = $frase;
-        
+        $aviso = "Ticket atualizado, e-mail enviado com sucesso!";
+        $_SESSION['aviso'] = $aviso;
+       
         $assunto = mysqli_real_escape_string($conexao, trim($_POST['assunto']));
         $descricao = mysqli_real_escape_string($conexao, trim($_POST['descricao']));
         $setor = mysqli_real_escape_string($conexao, trim($_POST['setor']));
@@ -20,14 +21,20 @@
         $email = mysqli_real_escape_string($conexao, trim($_POST['email']));
         $telefone = mysqli_real_escape_string($conexao, trim($_POST['telefone']));
         //$situacao = mysqli_real_escape_string($conexao, trim($_POST['situacao']));   
-        
+                
         $sqlInsert = "UPDATE ticket 
         SET assunto='$assunto', descricao='$descricao', setor='$setor', prioridade='$prioridade', usuario_id='$usuario_id', email='$email', telefone='$telefone', data_solicitacao= NOW()
         WHERE ticket_id=$ticket_id";
         $result = $conexao->query($sqlInsert);
         //print_r($result);
-    }
-   
-    header('Location: ../views/ticketLista.php');
 
+        #Busca o nome pelo id usuario cadastrado no ticket e chama a função de de envio do e-mail
+        $buscanome = "SELECT nome FROM usuario WHERE usuario_id = $usuario_id";
+        $result = mysqli_query($conexao, $buscanome);
+        $resultado = mysqli_fetch_assoc($result);
+        $nome = $resultado['nome'];
+        $acao = 'atualização de ticket';
+        enviarEmail($nome,$email,$acao);
+    }
+    header('Location: ../views/ticketLista.php');
 ?>
